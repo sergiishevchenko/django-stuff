2ce1357468f504c5452eccc4a23dbfd62e256267
sergii <sergiishevchenko017@gmail.com>
2018-11-30 21:14:26 +0300

    first commit

--- guestbook/models.py
+++ guestbook/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Comment(models.Model):
+    name = models.CharField(max_length=20)
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
